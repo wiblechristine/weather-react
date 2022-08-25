b3114e745beb7e1b3937be4a548e484e6b29a63a
Christine <midnighter07@gmail.com>
2022-08-25 11:53:26 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import WeatherSearch from "./WeatherSearch";
+
+import "./App.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <div className="App">
+      <h1>Weather App</h1>
+      <WeatherSearch />
+    </div>
+  </StrictMode>
+);
